93d5371540de3665224363bbd1eef7591baff773
Sanjay Govind <govindsanj@ecs.vuw.ac.nz>
2019-09-13 22:35:03 +1200

    Dont depend on protocollib

--- src/main/java/net/tangentmc/portalstick/Utils.java
+++ src/main/java/net/tangentmc/portalstick/Utils.java
@@ -2,18 +2,8 @@ package net.tangentmc.portalstick;
 
 import org.bukkit.block.BlockFace;
 import org.bukkit.util.Vector;
-import org.joml.Vector3d;
-
 public class Utils {
     private static final BlockFace[] RADIAL = {BlockFace.WEST, BlockFace.NORTH_WEST, BlockFace.NORTH, BlockFace.NORTH_EAST, BlockFace.EAST, BlockFace.SOUTH_EAST, BlockFace.SOUTH, BlockFace.SOUTH_WEST};
-
-    public static Vector3d from(Vector vector) {
-        return new Vector3d(vector.getX(), vector.getY(), vector.getZ());
-    }
-
-    public static Vector from(Vector3d vector) {
-        return new Vector(vector.x, vector.y, vector.z);
-    }
 
     public static BlockFace getDirection(Vector facing, Vector rotation) {
         if (facing.getX() == 0 && facing.getZ() == 0) {
